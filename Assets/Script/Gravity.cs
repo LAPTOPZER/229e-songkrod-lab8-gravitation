6930d1eb2d027055e4c57aaa42c2b2fd7887adb1
LAPTOPZER <vakadarbhafill@gmail.com>
2025-03-13 10:46:01 +0700

    Orbit + Force

--- Assets/Script/Gravity.cs
+++ Assets/Script/Gravity.cs
@@ -1,13 +1,38 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Gravity : MonoBehaviour
 {
     Rigidbody rb;
-    const float G = 0.00674f;
+    const float G = 0.0674f;
+    public static List<Gravity> gravityObjectsList;
+    [SerializeField] bool planet = false;
+    [SerializeField] int orbitSpeed = 10000;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        if (gravityObjectsList == null )
+        {
+            gravityObjectsList = new List<Gravity>();
+        }
+
+        gravityObjectsList.Add( this );
+        if (!planet)
+        {
+            rb.AddForce(Vector3.back * orbitSpeed);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        foreach ( var planet in gravityObjectsList)
+        {
+            if (planet != this)
+            {
+                Attract(planet);
+            }
+        }
     }
 
     void Attract(Gravity other)
